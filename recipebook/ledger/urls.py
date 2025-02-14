4699ea515e2f0847e2228010036029463df0280b
Gerardo Galicia <gerardo.galicia@student.ateneo.edu>
2025-02-14 12:45:32 +0800

    Changed branch to lab1

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, task_list
 # from .views import task_list
+
 urlpatterns = [
     path('', index, name='index'),
-    # path('', task_list, name=task_list),
+    path('task_list/', task_list, name='task_list'),
     ]
 
 app_name = 'ledger'
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -5,13 +5,14 @@ def index(request):
     return HttpResponse('Hello World! This came from the index view')
 # Create your views here.
 
-# def task_list(request):
-#     ctx = {
-# "tasks": [
-# "task 1",
-# "task 2",
-# "task 3",
-# "task 4",
-#     ]
-# }
-#     return (HttpResponse(render(request, 'task_list', ctx,)))
+def task_list(request):
+    ctx = {
+"tasks": [
+"task 1",
+"task 2",
+"task 3",
+"task 4",
+"task 5",
+    ]
+}
+    return (HttpResponse(render(request, 'task_list.html', ctx,)))
