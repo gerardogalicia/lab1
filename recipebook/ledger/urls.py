5a07999c5cbb3930f1bfd31ce7c3fbd8c48a4fbc
Gerardo Galicia <gerardo.galicia@student.ateneo.edu>
2025-02-14 23:20:00 +0800

    Playing around with html

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import index, task_list, recipe_1, recipe_2
+from .views import index, task_list, recipe_1, recipe_2, recipe_index
 
 urlpatterns = [
     path('', index, name='index'),
     path('task_list/', task_list, name='task_list'),
     path('Recipe_1/', recipe_1, name = 'Recipe_1'),
     path('Recipe_2/', recipe_2, name = 'Recipe_2'),
+    path('Recipe_index/', recipe_index, name = 'Recipe_index'),
     ]
 
 app_name = 'ledger'
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -17,6 +17,73 @@ def task_list(request):
 }
     return (HttpResponse(render(request, 'task_list.html', ctx,)))
 
+def recipe_index(request):
+    recipe_context ={
+    "recipes": [
+        {
+            "name": "Recipe 1",
+            "ingredients": [
+                {
+                    "name": "tomato",
+                    "quantity": "3pcs"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1kg"
+                },
+                {
+                    "name": "water",
+                    "quantity": "1L"
+                },
+                {
+                    "name": "sinigang mix",
+                    "quantity": "1 packet"
+                }
+            ],
+            "link": "/recipe/1"
+        },
+        {
+            "name": "Recipe 2",
+            "ingredients": [
+                {
+                    "name": "garlic",
+                    "quantity": "1 head"
+                },
+                {
+                    "name": "onion",
+                    "quantity": "1pc"
+                },
+                {
+                    "name": "vinegar",
+                    "quantity": "1/2cup"
+                },
+                {
+                    "name": "water",
+                    "quanity": "1 cup"
+                },
+                {
+                    "name": "salt",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "whole black peppers",
+                    "quantity": "1 tablespoon"
+                },
+                {
+                    "name": "pork",
+                    "quantity": "1 kilo"
+                }
+            ],
+            "link": "/recipe/2"
+        }
+    ]
+}
+    return (HttpResponse(render(request, 'Recipe_template.html', recipe_context,)))
+
 
 
 def recipe_1(request):
