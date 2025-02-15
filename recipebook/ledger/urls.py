b13c73e1ba17108ff44d566d42ac8a00e390e0f3
Gerardo Galicia <gerardo.galicia@student.ateneo.edu>
2025-02-15 11:47:48 +0800

    Removed Test urls and views

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import index, task_list, recipe_1, recipe_2, recipe_index
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('task_list/', task_list, name='task_list'),
     path('Recipe/1', recipe_1, name = 'Recipe_1'),
     path('Recipe/2', recipe_2, name = 'Recipe_2'),
     path('Recipes/list', recipe_index, name = 'Recipe_index'),
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('This is the index view. You are likely looking for something else.')
-# Create your views here.
-
-def task_list(request):
-    ctx = {
-"tasks": [
-"task 1",
-"task 2",
-"task 3",
-"task 4",
-"task 5",
-    ]
-}
-    return (HttpResponse(render(request, 'task_list.html', ctx,)))
-
 def recipe_index(request):
     recipe_context ={
     "recipes": [
