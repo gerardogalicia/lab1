e782d9c70b65635c371229230057852034fa53e0
Gerardo Galicia <gerardo.galicia@student.ateneo.edu>
2025-02-27 14:58:09 +0800

    Updated admin.py

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ingredient, Recipe, RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+# registering the model and the admin is what tells
+# Django that admin pages must be generated for the models specified
+admin.site.register(RecipeAdmin, IngredientAdmin ,RecipeIngredientAdmin)
+admin.site.register(Recipe, Ingredient, RecipeIngredient)
