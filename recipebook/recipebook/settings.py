afd8660a0e6fc0dcb2901f544f361a805ab83df7
Gerardo Galicia <gerardo.galicia@student.ateneo.edu>
2025-03-11 09:57:34 +0800

    Login is working now with custom views to boot

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -115,6 +115,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = "/recipes/list"
+LOGOUT_REDIRECT_URL = "/accounts/login"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
