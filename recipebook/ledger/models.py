8a14071380dcd029c3e6fcd48e9d4c5101228a44
Gerardo Galicia <gerardo.galicia@student.ateneo.edu>
2025-03-24 22:14:29 +0800

    Initial commit

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 
 
 
+
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     author = models.CharField(max_length=50)
